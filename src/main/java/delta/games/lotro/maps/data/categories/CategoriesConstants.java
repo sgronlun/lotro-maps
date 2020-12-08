b0b28f4bfe4568a11f2e3e790d606ea20bc94288
Damien Morcellet <dmorcellet@free.fr>
2020-12-08 19:03:56 +0100

    Added constants for categories

---
+++ src/main/java/delta/games/lotro/maps/data/categories/CategoriesConstants.java
@@ -0,0 +1,81 @@
+package delta.games.lotro.maps.data.categories;
+
+/**
+ * Constants for category codes.
+ * @author DAM
+ */
+public class CategoriesConstants
+{
+  /**
+   * Crafting Vendor.
+   */
+  public static final int CRAFTING_VENDOR=33;
+  /**
+   * Vendor.
+   */
+  public static final int VENDOR=38;
+  /**
+   * Trainer.
+   */
+  public static final int TRAINER=42;
+  /**
+   * Waypoint.
+   */
+  public static final int WAYPOINT=48;
+  /**
+   * Resource node.
+   */
+  public static final int RESOURCE_NODE=50;
+  /**
+   * Milestone.
+   */
+  public static final int MILESTONE=55;
+  /**
+   * Trader.
+   */
+  public static final int TRADER=58;
+  /**
+   * Faction representative.
+   */
+  public static final int FACTION_REPRESENTATIVE=61;
+  /**
+   * NPC.
+   */
+  public static final int NPC=70;
+  /**
+   * Monster.
+   */
+  public static final int MONSTER=71;
+  /**
+   * Container.
+   */
+  public static final int CONTAINER=72;
+  /**
+   * Item.
+   */
+  public static final int ITEM=73;
+  /**
+   * Landmark.
+   */
+  public static final int LANDMARK=74;
+  /**
+   * Hotspot.
+   */
+  public static final int HOTSPOT=75;
+  /**
+   * Door.
+   */
+  public static final int DOOR=76;
+  /**
+   * Crop.
+   */
+  public static final int CROP=77;
+  /**
+   * Critter.
+   */
+  public static final int CRITTER=78;
+  /**
+   * Other.
+   */
+  public static final int OTHER=100;
+}
