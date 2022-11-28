8cb96049ead5fa45fbe91e930157d5f237c04d43
Damien Morcellet <dmorcellet@free.fr>
2022-11-28 18:25:50 +0100

    Started the display selection table

---
+++ src/main/java/delta/games/lotro/maps/ui/displaySelection/table/DisplaySelectionDetailsColumnIds.java
@@ -0,0 +1,21 @@
+package delta.games.lotro.maps.ui.displaySelection.table;
+
+/**
+ * Column identifiers for the columns of the display selection details.
+ * @author DAM
+ */
+public enum DisplaySelectionDetailsColumnIds
+{
+  /**
+   * Visible.
+   */
+  VISIBLE,
+  /**
+   * Name.
+   */
+  NAME,
+  /**
+   * Count.
+   */
+  COUNT
+}
